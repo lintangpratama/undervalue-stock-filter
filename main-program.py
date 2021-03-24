f4dddfca261fbda519e966d27f50214968625003
Lintang Pratama <lintangajiyogapratama@gmail.com>
2021-03-24 13:41:22 +0700

    Add all emiten list from Indonesian Stock Exchange Market.

--- main-program.py
+++ main-program.py
@@ -1,29 +1,22 @@
 # Importing the library
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
+from emitenList import emiten_code
 
+# Initialize Scrapingdog API key
+# Scrappingdog is a web scrapper that let you used the scrapping API for free (max: 1000 request) 
+api_key = '60304c8757fd5d19cf3ddf3b'
 
-# Dictionary url for each emitent
-# You can add the url by change the emitent code, ex: ADHI.JK to BBCA.JK or something
-allurl = ["https://api.scrapingdog.com/scrape?api_key=ADD YOUR SCRAPPINGDOG API HERE&url=https://finance.yahoo.com/quote/ADHI.JK/key-statistics?p=ADHI.JK", 
-         "https://api.scrapingdog.com/scrape?api_key=ADD YOUR SCRAPPINGDOG API HERE&url=https://finance.yahoo.com/quote/PTRO.JK/key-statistics?p=PTRO.JK",
-         "https://api.scrapingdog.com/scrape?api_key=ADD YOUR SCRAPPINGDOG API HERE&url=https://finance.yahoo.com/quote/TBLA.JK/key-statistics?p=TBLA.JK",
-         "https://api.scrapingdog.com/scrape?api_key=ADD YOUR SCRAPPINGDOG API HERE&url=https://finance.yahoo.com/quote/IGAR.JK/key-statistics?p=IGAR.JK",
-         "https://api.scrapingdog.com/scrape?api_key=ADD YOUR SCRAPPINGDOG API HERE&url=https://finance.yahoo.com/quote/SMSM.JK/key-statistics?p=SMSM.JK",
-         "https://api.scrapingdog.com/scrape?api_key=ADD YOUR SCRAPPINGDOG API HERE&url=https://finance.yahoo.com/quote/MLPT.JK/key-statistics?p=MLPT.JK",
-         "https://api.scrapingdog.com/scrape?api_key=ADD YOUR SCRAPPINGDOG API HERE&url=https://finance.yahoo.com/quote/LINK.JK/key-statistics?p=LINK.JK",
-         "https://api.scrapingdog.com/scrape?api_key=ADD YOUR SCRAPPINGDOG API HERE&url=https://finance.yahoo.com/quote/TINS.JK/key-statistics?p=TINS.JK",
-         "https://api.scrapingdog.com/scrape?api_key=ADD YOUR SCRAPPINGDOG API HERE&url=https://finance.yahoo.com/quote/POWR.JK/key-statistics?p=POWR.JK",
-         "https://api.scrapingdog.com/scrape?api_key=ADD YOUR SCRAPPINGDOG API HERE&url=https://finance.yahoo.com/quote/BJTM.JK/key-statistics?p=BJTM.JK",
-         "https://api.scrapingdog.com/scrape?api_key=ADD YOUR SCRAPPINGDOG API HERE&url=https://finance.yahoo.com/quote/BBCA.JK/key-statistics?p=BBCA.JK"]
+# Initialize the emitent code from IDX Stock
+
 
 print('IHSG Undervalue Emitent (0<PER<15 and 0<PBV<1) List:')
 
-for i in range(len(allurl)):
+for i in range(len(emiten_code)):
     # Get the API url
     # Data get from Yahoo! Finance
     # Using ScrapingDog for scrap the data that we want to use
-    url = requests.get(allurl[i]).text
+    url = requests.get(f"https://api.scrapingdog.com/scrape?api_key={api_key}&url=https://finance.yahoo.com/quote/{emiten_code[i]}/key-statistics?p={emiten_code[i]}").text
     soup = BeautifulSoup(url, "html.parser")
     data = soup.find_all("tbody")
     name = soup.find_all("h1")
@@ -46,18 +39,25 @@ for i in range(len(allurl)):
        table_td_PBV = None
       
     # Add the PBV and PER data to the emitent dictionary
-    emiten['Name'] = name[0].text
-    emiten[table_td_PER[0].text] = float(table_td_PER[1].text)
-    emiten[table_td_PBV[0].text] = float(table_td_PBV[1].text)
-
+    try:
+      emiten['Name'] = name[0].text
+    except:
+      pass
     # Add an emitent data to IHSG list
+    try:
+      emiten[table_td_PER[0].text] = float(table_td_PER[1].text)
+      emiten[table_td_PBV[0].text] = float(table_td_PBV[1].text)
+    except:
+      pass
     ihsg.append(emiten)
 
-    # Filtering an undervalued emitent
-    if 0 <= ihsg[0]['Trailing P/E '] <= 15 and 0 <= ihsg[0]['Price/Book (mrq)'] <= 1:
-        print(ihsg[0]['Name'])
-    else:
-        print(ihsg[0]['Name'], "WAS OVERVALUED!")
+    try:
+      if 0 <= ihsg[0]['Trailing P/E '] <= 15 and 0 <= ihsg[0]['Price/Book (mrq)'] <= 1:
+          print('-', ihsg[0]['Name'])
+    except:
+      pass
 
+
+print('')
 print('Thanks for using this program :)')
 print('Data was got from Yahoo! Finance')
